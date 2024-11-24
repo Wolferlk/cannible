52d172a0dc9ec22a81d4f61f2d73846086e41860
wolferlk <sasiofficial25@gmail.com>
2024-11-24 18:33:17 +0530

    s

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Hero from '../components/Hero';
 import { products } from '../data/products';
 import ProductCard from '../components/ProductCard';
-import Scrolling from '../components/scrollimg';
+import Scrolling from '../components/Scrollimg';
 
 export default function Home() {
   const featuredProducts = products.slice(0, 3);
